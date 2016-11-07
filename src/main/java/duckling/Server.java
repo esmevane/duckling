df8abea03d80aa69a8ff86938d74fef52dd10142
Joseph McCormick <esmevane@gmail.com>
2016-11-07 15:29:09 -0600

    Ex nihilo.
    First commit of a small HTTP server from scratch, Duckling.

---
+++ src/main/java/duckling/Server.java
@@ -0,0 +1,5 @@
+package duckling;
+
+public class Server {
+
+}
---
+++ src/test/java/duckling/ServerTest.java
@@ -0,0 +1,13 @@
+//import static org.junit.Assert.assertEquals;
+//
+//import org.junit.Before;
+import org.junit.Test;
+
+import duckling.Server;
+
+public class ServerTest {
+    @Test
+    public void setPort() throws Exception {
+        Server server = new Server();
+    }
+}
